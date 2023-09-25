49a65d00fa6ce9b123385a28728dd5dd3ac21d72
Pranav Aher <84280592+pran7620@users.noreply.github.com>
2023-09-26 03:08:54 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,23 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2709
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 from setuptools import setup, find_packages\
+\
+setup(\
+    name='my_python_project',\
+    version='0.1.0',\
+    description='My Python Project Description',\
+    author='Your Name',\
+    author_email='your@email.com',\
+    packages=find_packages(),\
+    install_requires=[\
+        # Add 'matplotlib' to the list of dependencies\
+        'matplotlib',\
+        # Add other dependencies as needed\
+    ],\
+)\
+}
